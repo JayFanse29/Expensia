cffe596f66fe2ec21a37cd2430b6c46c95a1f4dc
Jay Fanse <99622128+JayFanse29@users.noreply.github.com>
2024-07-10 13:14:57 +0530

    204 res code

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,7 +4,7 @@ const corsOptions = {
     origin: 'https://expensia-frontend.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
-    optionsSuccessStatus: 200
+    optionsSuccessStatus: 204
 };
 
 module.exports = cors(corsOptions);
