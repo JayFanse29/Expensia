951b43b1cccffe770002880bdf917dcce0da5838
Jay Fanse <jayf29112003@gmail.com>
2024-07-10 13:37:15 +0530

    removed preflight option

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -3,8 +3,7 @@ const cors = require('cors');
 const corsOptions = {
     origin: 'https://expensia-frontend.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204
+    optionsSuccessStatus: 200
 };
 
 module.exports = cors(corsOptions);
